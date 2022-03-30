a9f28c593c8844eaa988e26d613cf26af962d25b
Himmatuz 31 <72717502+Himmatuz31@users.noreply.github.com>
2022-03-31 06:47:04 +0700

    Add files via upload

---
+++ no 5.py
@@ -0,0 +1,17 @@
+# Amalia Ramadhani Amanda Syafi'i
+# 20051397080
+# D4 MI 2020B
+
+class Stack(list):
+    push = list.append
+
+def modify_stack(symbolString):
+    stack=Stack()
+    result = []
+    for character in symbolString:
+        if character != '*':
+            stack.push(character)
+        else:
+            result.append(stack.pop())
+    return ''.join(result)
+print(modify_stack('EAS*Y*QUE***ST***IO*N***'))
